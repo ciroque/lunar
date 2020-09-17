9636a1e701e5b977d663dc391b84940d46394dd2
steve.wagner <steve.wagner@termly.io>
2020-09-17 14:01:26 -0700

    Wraps the initial implementation

--- lunar.cpp
+++ lunar.cpp
@@ -2,22 +2,32 @@
  * adapted from: http://www.voidware.com/phase.c
  * CalculateJulianDate from: https://www.programmingassignmenthelper.com/convert-a-date-to-the-julian-day-number-in-c/
  */
+#include "lunar.h"
 
 #include <math.h>
 
-#include "lunar.h"
+Phase Lunar::GetMoonPhase(unsigned int year, unsigned int month, double day) {
+    auto lunar = new Lunar();
+    auto phase = Phase{};
 
-std::list<Phase> Lunar::GetMoonPhases() {
-    return std::list<Phase>();
+    phase.julianDay = lunar->CalculateJulianDay(year, month, day);
+    phase.sunPosition = lunar->CalculatePositionOfSun(phase.julianDay);
+    phase.moonPosition = lunar->CalculatePositionOfMoon(phase.julianDay, phase.sunPosition);
+
+    lunar->CalculatePhaseOfMoon(&phase);
+
+    delete lunar;
+
+    return phase;
 }
 
-double Lunar::CalculateJulianDay(int year, int month, double day) {
+double Lunar::CalculateJulianDay(unsigned int year, unsigned int month, double day) {
     int a, m, y, leap_days;
     a = ((14 - month) / 12);
     m = (month - 3) + (12 * a);
     y = year + 4800 - a;
     leap_days = (y / 4) - (y / 100) + (y / 400);
-    return day + (((153 * m) + 2) / 5) + (365 * y) + leap_days - 32045;
+    return (day + (((153 * m) + 2) / 5) + (365 * y) + leap_days - 32045) - 2444238.5;
 }
 
 double Lunar::CalculatePositionOfSun(double j) {
@@ -41,4 +51,42 @@ double Lunar::CalculatePositionOfSun(double j) {
     i = l / 360;
     l = l - i * 360.0;
     return l;
+}
+
+double Lunar::CalculatePositionOfMoon(double julianDay, double sunPosition) {
+    double ms,l,mm,n,ev,sms,z,x,lm,bm,ae,ec;
+    double d;
+    double ds, as, dm;
+    int i;
+
+    /* ls = sun_position(j) */
+    ms = 0.985647332099 * julianDay - 3.762863;
+    if (ms < 0) ms += 360.0;
+    l = 13.176396 * julianDay + 64.975464;
+    i = l / 360;
+    l = l - i * 360.0;
+    if (l < 0) l += 360.0;
+    mm = l - 0.1114041 * julianDay - 349.383063;
+    i = mm / 360;
+    mm -= i * 360.0;
+    n = 151.950429 - 0.0529539 * julianDay;
+    i = n / 360;
+    n -= i * 360.0;
+    ev = 1.2739 * sin((2 * (l - sunPosition) - mm) * RAD);
+    sms = sin(ms * RAD);
+    ae = 0.1858 * sms;
+    mm += ev - ae - 0.37 * sms;
+    ec = 6.2886 * sin(mm * RAD);
+    l += ev + ec - ae + 0.214 * sin(2 * mm * RAD);
+    l= 0.6583 * sin(2 * (l - sunPosition) * RAD) + l;
+    return l;
+}
+
+void Lunar::CalculatePhaseOfMoon(Phase *phase) {
+    double t = phase->moonPosition - phase->sunPosition;
+    if (t < 0) t += 360;
+
+    phase->segment = (int)((t + 22.5)/45) & 0x7;
+    phase->visible = (1.0 - cos((phase->moonPosition - phase->sunPosition) * RAD)) / 2;
+    phase->name = (Name)phase->segment;
 }
--- lunar.h
+++ lunar.h
@@ -3,26 +3,40 @@
 
 #include <list>
 
-enum Direction {
-    WAXING = 1,
-    WANING = 2
+enum Name {
+    New = 0,
+    WaningCrescent,
+    ThirdQuarter,
+    WaningGibbous,
+    Full,
+    WaxingGibbous,
+    FirstQuarter,
+    WaxingCrescent
 };
 
 struct Phase {
-    Direction direction;
-    double Visible;
+    int julianDay;
+    double sunPosition;
+    double moonPosition;
+    int segment;
+    double visible;
+    Name name;
 };
 
 class Lunar {
 public:
-    static std::list<Phase> GetMoonPhases();
-    double CalculateJulianDay(int year, int month, double day);
-    double CalculatePositionOfSun(double julianDay);
+    static Phase GetMoonPhase(unsigned int year, unsigned int month, double day);
 
 private:
     const double PI = 3.1415926535897932384626433832795;
     const double RAD = (PI/180.0);
     const double SMALL_FLOAT = (1e-12);
+
+    double CalculateJulianDay(unsigned int year, unsigned int month, double day);
+    double CalculatePositionOfSun(double julianDay);
+    double CalculatePositionOfMoon(double julianDay, double sunPosition);
+//    double CalculatePhaseOfMoon(double julianDay, double sunPosition, double moonPosition);
+    void CalculatePhaseOfMoon(Phase *phase);
 };
 
 #endif //LUNAR_LUNAR_H
