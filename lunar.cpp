b748581086f6101cbb082abc18f0b030b8647940
steve.wagner <steve.wagner@termly.io>
2020-09-17 12:35:48 -0700

    Adds sun position calucalation

--- lunar.cpp
+++ lunar.cpp
@@ -1,11 +1,11 @@
 /*
  * adapted from: http://www.voidware.com/phase.c
+ * CalculateJulianDate from: https://www.programmingassignmenthelper.com/convert-a-date-to-the-julian-day-number-in-c/
  */
 
+#include <math.h>
 
 #include "lunar.h"
-
-#include <iostream>
 
 std::list<Phase> Lunar::GetMoonPhases() {
     return std::list<Phase>();
@@ -18,4 +18,27 @@ double Lunar::CalculateJulianDay(int year, int month, double day) {
     y = year + 4800 - a;
     leap_days = (y / 4) - (y / 100) + (y / 400);
     return day + (((153 * m) + 2) / 5) + (365 * y) + leap_days - 32045;
+}
+
+double Lunar::CalculatePositionOfSun(double j) {
+    double n, x, e, l, dl, v;
+    double m2;
+    int i;
+
+    n = 360 / 365.2422 * j;
+    i = n / 360;
+    n = n - i * 360.0;
+    x = n - 3.762863;
+    if(x < 0) x += 360;
+    x *= RAD;
+    e = x;
+    do {
+        dl = e - .016718 * sin(e) - x;
+        e = e - dl / (1-.016718 * cos(e));
+    } while (fabs(dl) >= SMALL_FLOAT);
+    v = 360 / PI * atan(1.01686011182 * tan(e / 2));
+    l = v + 282.596403;
+    i = l / 360;
+    l = l - i * 360.0;
+    return l;
 }
--- lunar.h
+++ lunar.h
@@ -17,8 +17,12 @@ class Lunar {
 public:
     static std::list<Phase> GetMoonPhases();
     double CalculateJulianDay(int year, int month, double day);
+    double CalculatePositionOfSun(double julianDay);
 
 private:
+    const double PI = 3.1415926535897932384626433832795;
+    const double RAD = (PI/180.0);
+    const double SMALL_FLOAT = (1e-12);
 };
 
 #endif //LUNAR_LUNAR_H
